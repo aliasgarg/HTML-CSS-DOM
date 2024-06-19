a54247da85fa36e82b6214ae18c856d236978b13
NoobPro004 <himanshugarg738@gmail.com>
2024-06-19 18:02:09 +0530

    added upto a.js

---
+++ a.js
@@ -0,0 +1,273 @@
+// let number = 83;
+// let flag = true;
+// let i;
+// for( i = 2; i<number; i++ ) {
+//     let rem=number%i;
+//     if(rem==0) {
+//         flag=false;
+//         break;
+//     }
+// }
+// if(flag==true) {
+//     console.log("prime hai");
+// }
+// else if(flag==false){
+//     console.log("prime nahi hai")
+// }
+
+
+
+// let a=[1,5,6,7,8]
+
+// for(let i of a){
+//     console.log(i)
+// }
+
+
+// let myObj = {
+
+//     name: "vishal",
+
+//     age: 22,
+
+//     location: "palwal",
+    
+//     email: "vishalgarg@gamil.com",
+    
+//     phoneNumber: 9079459103,
+// neNumber: 9079459103,
+
+//     func: ()=>{
+//         let a='hello'
+//         console.log(a)
+//     }
+
+// }
+
+// myObj.email = "vishu@google.com";
+// Object.freeze(myObj)
+// myObj.email = "v123456789@gmail.com"
+// console.log(myObj)
+
+// myObj.func()
+
+// let number = 83;
+// let flag = true;
+// let i;
+// for( i = 2; i<number; i++ ) {
+//     let rem=number%i;
+//     if(rem==0) {
+//         flag=false;
+//         break;
+//     }
+// }
+// if(flag==true) {
+//     console.log("prime hai");
+// }
+// else if(flag==false){
+//     console.log("prime nahi hai")
+// }
+
+// 
+
+// let Temperature = 45;
+
+// if (Temperature <=60 ) {
+//     console.log("temperature 60 se kam hai ")
+// }
+
+// else  {
+// console.log("temperature jayda hai")
+// }
+
+// if(1=="1"){
+//     console.log("hwllo")
+// }\||
+// let myStr = ["a", "b", "c", "d", "e", "f", "g"];
+
+// switch (myStr[4]) {
+//     case "d":
+//         console.log("d")
+        
+//     case "e":
+//         console.log("e")
+//        break
+//     case "h":
+//         console.log("h")
+//         break
+//     default:
+//         console.log("else ")
+//         break;
+// }
+
+
+// for( i =1; i<=45; i++) {
+//     let rem = i%3 ;
+//    if (rem == 0){
+//     console.log(`number is multiple of 3 ${i}`);
+//     continue
+//    }
+// }
+
+
+
+
+// let a = [1,2,3,4] ?? undefined  ?? "himanshu";
+
+// console.log(a)
+
+
+// let obj={
+//     "IN":"India",
+//     "USA":"United States of America"
+// }
+
+
+// let map = new Map()
+// map.set('IN',"Inida")
+// map.set("USA","United States of America")
+// console.log(map.get("IN"))
+
+
+// map.set("IN","Australia")
+// console.log(map.get("IN"))
+
+// map.set("IN","Sri Lanka")
+// console.log(map.get("IN"))
+
+
+// let myArr = [
+//   obj1 = { 
+//         location:"palwal",
+//         name: "vishal",
+//     },
+
+//   obj2 = {
+//         name: "himanshu",
+//         age: 25,
+//     },
+
+//   obj3 = {
+//         user: "urmila",
+//         phoneNumber: 123456789,
+
+//     }
+
+// ]    
+// myArr.forEach( (obj1, index) => {
+//     console.log(obj1)
+//     if(index==1){
+//         console.log(obj1.name)
+//     }
+// } )
+
+// const books = [
+//     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//     { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//     { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//     { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//     { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//     { title: 'Book Six', genre: 'Non-Fiction', publish: 1987, edition: 2010 },
+//     { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+//     { title: 'Book Eight', genre: 'Non-Fiction', publish: 2011, edition: 2016 },
+//     { title: 'Book Nine', genre: 'Non-Fiction', publish: 2005, edition: 1989 },
+//   ];
+// let userBooks = books.filter( (Books) => {
+//     return Books.genre === "Fiction" && Books.publish > 1990 && Books.edition >= 2005
+// }  ) 
+
+// console.log(userBooks)
+
+
+let arr1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; 
+let arr2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26];
+// let ans=0;
+// for(let i=0; i<arr1.length; i++){
+//  ans=ans+arr1[i]
+// }
+// console.log(ans)
+
+// let ans2=0;
+// for(let i=0; i<arr2.length; i++){
+//   ans2=ans2+arr2[i]
+// }
+// console.log(ans2)
+// console.log(ans+ans2)
+
+// let user1= arr1.map((val1) => {
+//   return 
+
+
+// }  )
+// not include 4
+// let n1 = 22;
+// for(i=0; i<=22; i++){
+//   if(i==4 || i==0){
+//     continue
+//   }
+//   let rem =i%2;
+//   rem==0;
+//   if(rem==0){
+//     console.log(i)
+//   }
+// }
+
+
+// let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+// let a=[]
+// for(let i=8; i>=0; i--){
+//   // console.log(arr[i])
+//   a.push(arr[i])
+// }
+// console.log(a)
+
+
+
+// let arr_3   = [4, 6, 7];
+// let arr_4    = [8, 1, 9];
+// for(let i=0; i)
+
+
+// let str1 = "javascript";
+// for(let i=1;i<=str1.length; i=+2){
+  
+// }
+
+
+
+// let str = "don’t know why";
+// let hai=false
+// for(let i=0; i<str.length; i++){
+//   let ans= "y" ;
+//   if(ans === str[i]){
+//     hai=true
+//     break
+//   }
+
+// }
+
+// if(hai==true){
+//   console.log("y hai")
+// }
+
+
+// for(let i=0;i<arr.length;i++){
+//   ans.push(arr[i] + arr2[i])
+// }
+
+
+// console.log(ans)
+
+// let str = "don’t know why"
+// let nstr=""
+// for(let i=0;i<str.length;i++){
+//   if(i%2==0){
+//     nstr=nstr+'y'
+//   }else{
+//     nstr=nstr+str[i]
+//   }
+// }
+
+// console.log(nstr)
+
+
